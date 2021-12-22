ae059943290bc76683cce3ae53b7d641a07feb71
Peter Weidenbach <weidenba@gmail.com>
2021-12-22 09:43:29 +0100

    parse_log_data implemented

--- src/modules/parsing.py
+++ src/modules/parsing.py
@@ -1,10 +1,18 @@
-def parse_log_header(log_file_lines, seperator=','):
-    return log_file_lines[0].split(seperator)
+def parse_log_header(log_header_line: list, seperator: str = ','):
+    return log_header_line.split(seperator)
 
 
-def parse_log_data_line(input_string, header_fields, seperator=','):
+def parse_log_data_line(input_data: str, header_fields: list, seperator: str = ',') -> str:
     result = dict()
-    values = input_string.split(seperator)
+    values = input_data.split(seperator)
     for index in range(len(header_fields)):
         result[header_fields[index]] = values[index]
+    return result
+
+
+def parse_log_data(input_data: list, seperator: str = ',') -> list:
+    header_fields = parse_log_header(input_data[0], seperator=seperator)
+    result = list()
+    for log_data_line in input_data[1:]:
+        result.append(parse_log_data_line(log_data_line, header_fields, seperator=seperator))
     return result
--- src/test/test_parsing.py
+++ src/test/test_parsing.py
@@ -1,11 +1,12 @@
-from modules.parsing import parse_log_header, parse_log_data_line
+from modules.parsing import parse_log_header, parse_log_data_line,\
+    parse_log_data
 
 TEST_LOG_LINES_INCLUDING_HEADER = ['First Value,Second Value,Third Value', '1,2,3', '1a,2a,3a']
 PARSED_LOG_HEADER = ['First Value', 'Second Value', 'Third Value']
 
 
 def test_parse_log_header():
-    result = parse_log_header(TEST_LOG_LINES_INCLUDING_HEADER)
+    result = parse_log_header(TEST_LOG_LINES_INCLUDING_HEADER[0])
     assert result == PARSED_LOG_HEADER
 
 
@@ -14,3 +15,11 @@ def test_parse_log_data_line():
     assert result['First Value'] == '1'
     assert result['Second Value'] == '2'
     assert result['Third Value'] == '3'
+
+
+def test_parse_log_data():
+    result = parse_log_data(TEST_LOG_LINES_INCLUDING_HEADER)
+    assert type(result) == list
+    assert len(result) == 2
+    assert result[0]['First Value'] == '1'
+    assert result[1]['Third Value'] == '3a'
